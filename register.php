a6ade2c9b934640d92f7e276635111684ae0ad24
DanWilliams24 <01danielwilliams@gmail.com>
2018-07-18 00:38:15 -0400

    added script connecting to localhost

--- register.php
+++ register.php
@@ -29,8 +29,26 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     #"mysql_real_escape_string" prevents inputs from SQL Injections.
     $username = mysql_real_escape_string($_POST['username']);
     $password = mysql_real_escape_string($_POST['password']);
+    $bool = true;
 
-    echo "Username entered is: ". $username . "<br>";
-    echo "Password entered is: ". $password;
+
+    mysql_connect("localhost", "root", "") or die(mysql_error()); //Connect to database
+    mysql_select_db("first_db") or die("Cannot connect to database");
+    $query = mysql_query("Select * from users");
+    while($row = mysql_fetch_array($query)){
+        $table_users = $row['username'];
+        if($username == $table_users){
+            $bool = false;
+            Print '<script>alert("Username has been taken!");</script>';
+            Print '<script>window.location.assign("register.php");</script>';
+        }
+    }
+
+    if($bool){
+        mysql_query("INSERT INTO users (username, password) VALUES ('$username','$password')");
+        Print '<script>alert("Successfully Registered!");</script>';
+        Print '<script>window.location.assign("register.php");</script>';
+    }
 }
+
 ?>
