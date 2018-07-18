69f25f023dc87c51a489513ddabc0b127a47acb7
DanWilliams24 <01danielwilliams@gmail.com>
2018-07-18 00:18:55 -0400

    set input type to secure text entry for password field

--- register.php
+++ register.php
@@ -14,7 +14,7 @@
     <h2>Registration Page</h2>
     <a href="index.php">Click here to go back</a><br><br>
     <form action="register.php" method="POST">
-        Enter Username: <input type="text" name="username" required="required"> <br> Enter Password: <input type="text" name="password" required="required"> <br>
+        Enter Username: <input type="text" name="username" required="required"> <br> Enter Password: <input type="password" name="password" required="required"> <br>
         <input type="submit" value="Register">
     </form>
 
@@ -26,6 +26,7 @@
 
 <?php
 if($_SERVER["REQUEST_METHOD"] == "POST"){
+    #"mysql_real_escape_string" prevents inputs from SQL Injections.
     $username = mysql_real_escape_string($_POST['username']);
     $password = mysql_real_escape_string($_POST['password']);
 
