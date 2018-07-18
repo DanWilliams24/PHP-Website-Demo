2f481e4a84f28f987e0f99dc20cbb89aef2edd17
DanWilliams24 <01danielwilliams@gmail.com>
2018-07-18 00:12:38 -0400

    added POST method for user credentials

--- register.php
+++ register.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,13 +9,27 @@
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
 
 </head>
+
 <body>
     <h2>Registration Page</h2>
     <a href="index.php">Click here to go back</a><br><br>
     <form action="register.php" method="POST">
-        Enter Username: <input type="text" name="username" required="required"> <br>
-        Enter Password: <input type="text" name="password" required="required"> <br>
+        Enter Username: <input type="text" name="username" required="required"> <br> Enter Password: <input type="text" name="password" required="required"> <br>
         <input type="submit" value="Register">
     </form>
+
+
 </body>
+
 </html>
+
+
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $username = mysql_real_escape_string($_POST['username']);
+    $password = mysql_real_escape_string($_POST['password']);
+
+    echo "Username entered is: ". $username . "<br>";
+    echo "Password entered is: ". $password;
+}
+?>
